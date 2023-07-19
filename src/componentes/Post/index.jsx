da207b5e8d5731ed6f129cbe6161a5575b8bc456
Rafael "Zipão" Silva <zipao.silva@gmail.com>
2023-07-19 01:22:21 -0300

    aula4.2

--- src/componentes/Post/index.jsx
+++ src/componentes/Post/index.jsx
@@ -1,16 +1,12 @@
-import styles from './Post.module.css';
+import { useParams } from "react-router-dom"
 
-export default function Post( { post}) {
+export default function Post() {
+  const parametros = useParams();
+
+  console.log(parametros);
+  
   return (
-    <div className={styles.post}>
-        <img
-            className={styles.capa}
-            src={`/assets/posts/${post.id}/capa.png`}
-            alt="Imagem de capa do post"
-        />
-        <h2 className={styles.titulo}>{post.titulo}</h2>
-
-        <button className={styles.botaoLer}>Ler</button>
-    </div>
+    <h1>Post {parametros.id}</h1>
   )
 }
+
