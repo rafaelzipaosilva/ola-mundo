0376e14e9caf1e9124d76aa79e4927a1eb0e31b3
Rafael "Zipão" Silva <zipao.silva@gmail.com>
2023-07-18 22:08:58 -0300

    aula3.2

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
 import Menu from "./componentes/Menu";
+import Rodape from "componentes/Rodape";
 
 function AppRoutes() {
   return (
@@ -13,6 +14,9 @@ function AppRoutes() {
         <Route path="/SobreMim" element={<SobreMim />} />
         <Route path="*" element={<div>Página não encontrada</div>} />
       </Routes>
+
+      <Rodape />
+      
     </BrowserRouter>
   )
 }
